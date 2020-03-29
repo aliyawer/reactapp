c3a7aaa1cff91e5f3fb350a6557be646f9d9b67a
aliyawer <aliyawerqambari@gmail.com>
2020-03-29 10:07:01 +0200

    my first app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Wellcome to Aliyawer Qambari's first react application.
+      </header>
+    </div>
+  );
+}
+
+export default App;
